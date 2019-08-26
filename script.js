5a71586c8f24089b3a9fba81f27061e1541a6345
adeoye adela <adeoye.adela@gmail.com>
2019-08-26 13:09:02 +0100

    Working on the Js file, arrays of suits and values created

---
+++ script.js
@@ -0,0 +1,23 @@
+/**Blackjack
+ * by Adela Adeoye
+ * a follow course by Mark Zamoyta
+ * github::github.com/adelaadeoye
+ */
+
+let suits = ['Heart', 'Clubs', 'Diamond', 'Spades'],
+    values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+        'Five', 'Four', 'Three', 'Two'
+    ];
+let deck = [];
+for (let suitsIdx = 0; suitsIdx < suits.length; suitsIdx++) {
+    for (let valuesIdx = 0; valuesIdx < values.length; valuesIdx++) {
+        deck.push(`${values[valuesIdx]} of ${suits[suitsIdx]} `)
+    }
+}
+console.log('Welcome to Blackjack by Adela');
+for (i = 0; i < deck.length; i++) {
+    console.log(deck[i]);
+}
+// console.log('You are dealt: ');
+// console.log(' ' + playerCards[0]);
+// console.log(' ' + playerCards[1]);
