6f1215fbe767a66b4177dd434af2673778457859
adeoye adela <adeoye.adela@gmail.com>
2019-08-26 13:40:01 +0100

    I added Function to the BlackJack Game

--- script.js
+++ script.js
@@ -8,16 +8,24 @@ let suits = ['Heart', 'Clubs', 'Diamond', 'Spades'],
     values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six',
         'Five', 'Four', 'Three', 'Two'
     ];
-let deck = [];
-for (let suitsIdx = 0; suitsIdx < suits.length; suitsIdx++) {
-    for (let valuesIdx = 0; valuesIdx < values.length; valuesIdx++) {
-        deck.push(`${values[valuesIdx]} of ${suits[suitsIdx]} `)
+createDeck = () => {
+    let deck = [];
+    for (let suitsIdx = 0; suitsIdx < suits.length; suitsIdx++) {
+        for (let valuesIdx = 0; valuesIdx < values.length; valuesIdx++) {
+            deck.push(`${values[valuesIdx]} of ${suits[suitsIdx]} `)
+        }
     }
+    return deck;
 }
+getNextCard = () => {
+    return deck.shift();
+}
+let deck = createDeck();
 console.log('Welcome to Blackjack by Adela');
 for (i = 0; i < deck.length; i++) {
     console.log(deck[i]);
 }
-// console.log('You are dealt: ');
-// console.log(' ' + playerCards[0]);
-// console.log(' ' + playerCards[1]);
+let playerCards = [getNextCard(), getNextCard()];
+console.log('You are dealt: ');
+console.log(' ' + playerCards[0]);
+console.log(' ' + playerCards[1]);
